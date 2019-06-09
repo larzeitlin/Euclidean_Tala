fe6576000f1d975dbbd89e5921034696eb9df164
larzeitlin <larzeitlin@gmail.com>
2019-06-09 18:29:46 +0100

    in a working state

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [quil "3.0.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [overtone "0.10.3"]
+                 [quil "3.0.0"]]
+ ; :repl-options {:init-ns er2.core}
+  :jvm-opts ^:replace [])
--- src/er2/core.clj
+++ src/er2/core.clj
@@ -2,17 +2,35 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+; For some reason this stops quil from killing internal sc server
+(q/defsketch dummy :size [10 10])
+(quil.applet/applet-disposed dummy)
+(use 'overtone.live)
+
 (def beats-temp 6)
 (def frame-rate 30)
 (def TAU (* 2 q/PI))
-(def bpm 140)
+(def bpm 220)
 (def ms-per-beat (/ 60000 bpm))
-(def colors {:blue        [102 153 204]
-             :yellow      [225 242 117]
-             :orange      [225 140 66]
-             :red         [225 60 56]
-             :dark-red    [162 62 72]
-             :alpha-gray1 [255 255 255 20]})
+
+;-----------------------> | R | G | B | A |
+(def colors {:blue        [102 153 204 255]
+             :yellow      [225 242 117 255]
+             :orange      [225 140 66  255]
+             :red         [225 60  56  255]
+             :dark-red    [162 62  72  255]
+             :alpha-gray1 [255 255 255 20 ]})
+
+(def samples-ids
+  {:tu          56153 ;1
+   :ge          56146 ;1
+   :xylophone   374699
+   :berimbau    177068
+   :hang-2      380509})
+
+(def sound-a (freesound (:tu samples-ids)))
+(def sound-b (freesound (:ge samples-ids)))
+(def sound-c (freesound (:berimbau samples-ids)))
 
 (defn create-circle-states [& names]
   (into {} (for [n names]
@@ -27,17 +45,30 @@
 (defn setup []
   (q/frame-rate frame-rate)
   (q/color-mode :rgb)
-  (merge {:control ::a   :frame-rate 30} {}
+  (merge {:control        ::a
+          :frame-rate     30
+          :mode           ::poly
+          :global-n-beats 8}
+        
          (create-circle-states ::a ::b ::c)))
+
+(defn bjorklund-kernel [v]
+  (if (apply = v) (flatten v)
+      (let [grouped (->> v (group-by identity) vals sort)
+            group-small (first grouped)
+            group-big (last grouped)
+            overlap (->> group-big (split-at (count group-small)) second (map vector))
+            output (concat (map vector group-big group-small) overlap)]
+        (if (> 2 (count group-small)) (flatten output)
+            (recur output)))))
 
 (defn bjorklund
   ([n k on-v off-v]
-   (let [full-gap (int (/ (- n k) k))
-         overlap (mod (- n k) k)]
-     (vec (-> (for [on (take k (repeat on-v))]
-                (conj (take full-gap (repeat off-v)) on))
-              (concat (take overlap (repeat off-v)))
-              flatten))))
+   (cond (zero? k) (take n (repeat off-v))
+         (= k n) (take n (repeat on-v))
+         :else (let [initial-vector (concat (take k (repeat on-v))
+                                            (take (- n k) (repeat off-v)))]
+                 (bjorklund-kernel initial-vector))))
   ([n k]
    (bjorklund n k 1 0)))
 
@@ -52,16 +83,21 @@
        (= (q/key-as-keyword) keyword)
        (apply = true bools)))
 
-(defn update-circle [state control]
-  (let [new-beat (mod (int (/ (q/millis) ms-per-beat))
-                      (:n-beats state))
-        new-n-beats (cond (key-control :up control)   (inc (:n-beats state))
-                          (key-control :down control) (dec (:n-beats state))
-                          :else                       (:n-beats state))
+(defn update-circle [state control sound mode new-global-n-beats]
+  (let [n-beats (if (= mode ::locked) new-global-n-beats
+                                      (:n-beats state))
+        new-beat (mod (int (/ (q/millis) ms-per-beat)) n-beats)
+        new-n-beats (cond (key-control :up control (= mode ::poly))   (inc n-beats)
+                          (key-control :down control (= mode ::poly)) (max (dec n-beats) 1)
+                          (= mode ::locked) new-global-n-beats
+                          :else                       n-beats)
         new-n-hits (cond (key-control :right control) (min (inc (:n-hits state)) new-n-beats)
                          (key-control :left control)  (max (dec (:n-hits state)) 1)
                          :else                        (:n-hits state))
         rotation-target (* new-beat (/ TAU new-n-beats))]
+
+    (when (and (not= new-beat (:current-beat state))
+               (nth (:bjorklund state) new-beat false)) (sound))
     
     {:n-beats new-n-beats
      :prev-beat (:current-beat state)
@@ -70,7 +106,7 @@
      :rotation-target rotation-target
      :n-hits new-n-hits
      :bjorklund (if (and (= new-n-hits (:n-hits state))
-                         (= new-n-beats (:n-beats state)))
+                         (= new-n-beats n-beats))
                   (:bjorklund state)
                   (bjorklund new-n-beats new-n-hits true false))}))
 
@@ -79,20 +115,39 @@
                       (key-control :a) ::a
                       (key-control :b) ::b
                       (key-control :c) ::c
-                      :else (:control state))]
+                      :else (:control state))
+        new-mode (cond (key-control :p) ::poly
+                       (key-control :l) ::locked
+                       :else (:mode state))
+        new-global-n-beats (cond (key-control :up (= ::locked (:mode state)))
+                                 (inc (:global-n-beats state))
+                                 
+                                 (key-control :down (= ::locked (:mode state)))
+                                 (max (dec (:global-n-beats state)) 1)
+                                 
+                                 :else
+                                 (:global-n-beats state))]
     (assoc state
-           ::a (update-circle (::a state) (= ::a new-control))
-           ::b (update-circle (::b state) (= ::b new-control))
-           ::c (update-circle (::c state) (= ::c new-control))
-           :control new-control)))
+           ::a (update-circle (::a state) (= ::a new-control) sound-a (:mode state) new-global-n-beats)
+           ::b (update-circle (::b state) (= ::b new-control) sound-b (:mode state) new-global-n-beats)
+           ::c (update-circle (::c state) (= ::c new-control) sound-c (:mode state) new-global-n-beats)
+           :control new-control
+           :mode new-mode
+           :global-n-beats new-global-n-beats)))
 
 (defn draw-circle [state rad col]
   (let [beat-arc (/ (* 1.5 q/PI) (:n-beats state)) ]
     (doseq [n (range (:n-beats state))]
       (q/with-rotation [(:rotation state)]
         (q/with-rotation [(* n (/ TAU (:n-beats state)))]
-          (if (nth (:bjorklund state) n false) (apply q/fill col)
-              (apply q/fill (:alpha-gray1 colors)))
+          (cond
+            (nth (:bjorklund state) (mod (inc (-  (dec (:n-beats state))
+                                                  (mod n (:n-beats state))))
+                                         (:n-beats state)) false)
+            (apply q/fill col)
+            
+            :else (apply q/fill (:alpha-gray1 colors)))
+          
           (q/arc 0 0 rad rad (* -0.5 beat-arc) (* 0.5 beat-arc) :pie)))))
   (q/fill 50)
   (q/stroke 50)
@@ -110,8 +165,7 @@
       (draw-circle (::c state) 180 (:yellow colors)))
     (q/fill 200)
     (q/text-size 20)
-    (q/text-align :center :center)
-    (q/text (str bpm) 0 0)))
+    (q/text-align :center :center)))
 
 (q/defsketch er2
   :title ""
@@ -121,4 +175,3 @@
   :draw draw-state
   :features [:keep-on-top]
   :middleware [m/fun-mode])
-
